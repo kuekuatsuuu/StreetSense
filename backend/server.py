93461ba64e9a457d17a99e716120fe973b6dd831
4kazch <akashabrah@gmail.com>
2025-04-09 19:26:15 +0530

    render

--- backend/server.py
+++ backend/server.py
@@ -80,4 +80,9 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=os.environ.get("FLASK_ENV") == "development"
+    )
+
